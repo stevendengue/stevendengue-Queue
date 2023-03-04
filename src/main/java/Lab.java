98bdb054918f56a9a06b1196a9de18135d864dd4
Steve Ndengue <steve.ndengue@gmail.com>
2023-03-04 21:13:36 +0000

    Lab: Queue complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,6 @@
 import java.util.Deque;
 import java.util.Queue;
+import java.util.ArrayDeque;
 
 /**
  * Queues are FIFO - first in, first out, like a checkout line. They are used in algorithms where it is needed to keep
@@ -36,7 +37,8 @@ public class Lab {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        Deque<String> queue = new ArrayDeque<String>();
+        return queue;
     }
 
     /**
@@ -45,7 +47,7 @@ public class Lab {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        return queue.size();
     }
 
     /**
@@ -55,6 +57,7 @@ public class Lab {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
+        queue.add(item);
 
     }
 
@@ -65,7 +68,8 @@ public class Lab {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        String rmitem = queue.remove();
+        return rmitem;
     }
 
     /**
@@ -75,6 +79,6 @@ public class Lab {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
